243c3e9367d67abc073be4b5ca8fbae9b8ddfe1b
Thomas Johansen <thomasjo@gmail.com>
2014-06-18 22:37:01 +0200

    Fix indentation according to PEP-8

--- symlink.py
+++ symlink.py
@@ -17,28 +17,28 @@ import re
 from docopt import docopt
 
 def main():
-  arguments = docopt(__doc__)
-  create_symlinks(arguments["--force"])
+    arguments = docopt(__doc__)
+    create_symlinks(arguments["--force"])
 
 def create_symlinks(force=False):
-  for root, dirs, files in os.walk("_/"):
-    target_root = os.path.expanduser(re.sub(r"_/", "~/", root))
-    if not os.path.isdir(target_root):
-      print("Creating empty directory {0}".format(target_root))
-      os.mkdir(target_root)
+    for root, dirs, files in os.walk("_/"):
+        target_root = os.path.expanduser(re.sub(r"_/", "~/", root))
+        if not os.path.isdir(target_root):
+            print("Creating empty directory {0}".format(target_root))
+            os.mkdir(target_root)
 
-    for f in files:
-      source = os.path.join(root, f)
-      target = os.path.join(target_root, f)
-      if os.path.islink(target) and force:
-        print("Removing existing symlink {0}".format(target))
-        os.remove(target)
-      else:
-        print("Skipping {0}".format(source))
-        continue
+        for f in files:
+            source = os.path.join(root, f)
+            target = os.path.join(target_root, f)
+            if os.path.islink(target) and force:
+                print("Removing existing symlink {0}".format(target))
+                os.remove(target)
+            else:
+                print("Skipping {0}".format(source))
+                continue
 
-      print("Creating symlink {0} -> {1}".format(source, target))
-      os.symlink(source, target)
+            print("Creating symlink {0} -> {1}".format(source, target))
+            os.symlink(source, target)
 
 if __name__ == "__main__":
-  main()
+    main()
